0bd50919a6f41e856dd893bcd04cdbea639f3113
WhoIN Lee <whoinlee@gmail.com>
2023-09-04 22:38:46 -0400

    App.test clean up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-// jest.useFakeTimers();
 describe('main page', () => {
   it('renders page heading', async () => {
     render(<App />);
@@ -11,13 +10,6 @@ describe('main page', () => {
   });
 
   it('does a search correctly', async () => {
-    /*
-    render(<App />);
-    expect(await screen.findByText(/Tokyo/)).toBeInTheDocument();
-    const textInput = screen.getByRole('textbox', { name: 'Search' });
-    userEvent.type(textInput, 'osaka');
-    expect(screen.queryByText(/Tokyo/)).not.toBeInTheDocument();
-    */
     render(<App />);
     expect(await screen.findByText(/Tokyo/)).toBeInTheDocument();
     const textInput = screen.getByRole('textbox', { name: 'search' }); 
